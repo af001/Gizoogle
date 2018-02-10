075fbfce04daab9d3ffbf323ffd6e61ccfa41ce1
Anton <af001@users.noreply.github.com>
2018-02-10 09:24:18 -0500

    Update gizoogle.py

--- gizoogle.py
+++ gizoogle.py
@@ -546,9 +546,10 @@ IMAGE_STORAGE_BUCKET = 'image_dump' # Change this to your image bucket
 AUDIO_STORAGE_BUCKET = 'speech_dump' # Change this to your speech bucket
 DOCUMENT_STORAGE_BUCKET = 'document_dump' # Change this to your document bucket
 VIDEO_STORAGE_BUCKET = 'video_dump' # Change this to your video bucket
+PROJECT_ID = 'analysis-283736' # Change this to your project-ID
+
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'flac', 'txt'])
-PROJECT_ID = 'analysis-283736' # Change this to your project-ID
 LANGUAGE = {
         "af-ZA": "Afrikaans (South Africa)", 
         "am-ET": "Amharic (Ethiopia)", 

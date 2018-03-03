b9cf20a045039a0ee8bdea63f6edcb3c2c31b003
Anton <af001@users.noreply.github.com>
2018-03-02 20:38:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,44 +18,8 @@ python --version
 ffmpeg -version | grep ffmpeg
 echo
 
-# Create a Google Cloud Platform project and credential file
-echo "[+] Create a project on the Google Cloud Platform"
-echo "[+] Create a service account key (json): APIs & Services --> Credentials"
-echo "[+] Put the key in your project path"
-echo 
-
 # Install gsutil command line tools and install
 echo "[+] Downloading and installing gsutils"
 curl https://sdk.cloud.google.com | bash
 exec -l $SHELL
-echo
-echo "[+] Run the following commands from the terminal:"
 
-# Initialize gsutil and create buckets with the appropriate permissions
-echo "# Initialize gsutil"
-echo "gsutil init"
-echo
-echo "# Create 4 new buckets - must have unique names"
-echo "gsutil mb gs://<a_video_bucket_name>"
-echo "gsutil mb gs://<a_audio_bucket_name>"
-echo "gsutil mb gs://<a_image_bucket_name>"
-echo "gsutil mb gs://<a_document_bucket_name>"
-echo
-echo "# Show the buckets"
-echo "gsutil ls"
-echo
-echo "# Change permissions to world readable for each bucket"
-echo "gsutil iam ch allUsers:objectViewer gs://<video_bucket_name>"
-echo "gsutil iam ch allUsers:objectViewer gs://<audio_bucket_name>"
-echo "gsutil iam ch allUsers:objectViewer gs://<image_bucket_name>"
-echo "gsutil iam ch allUsers:objectViewer gs://<document_bucket_name>"
-echo
-echo "# Set lifecycle to delete files older than 10 days in each bucket"
-echo "gsutil lifecycle set bucket_config.json gs://<video_bucket_name>"
-echo "gsutil lifecycle set bucket_config.json gs://<audio_bucket_name>"
-echo "gsutil lifecycle set bucket_config.json gs://<image_bucket_name>"
-echo "gsutil lifecycle set bucket_config.json gs://<document_bucket_name>"
-echo 
-echo
-echo "[+] Usage: python2.7 gizoogle.py"
-echo "[+] Terminal Usage: help"

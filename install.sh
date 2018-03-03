c8469e5fd4874049ce7d4a0ab3f62536b4082b98
Anton <af001@users.noreply.github.com>
2018-03-02 20:29:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@
 
 # Update linux
 apt-get update && apt-get -y upgrade
-apt-get -y install python python-pip ffmpeg build-essential
+apt-get -y install python python-pip ffmpeg build-essential curl
 
 # Install Python packages --> change to requirments.txt
 echo "[+] Installing Python packages..."
-pip install --upgrade setuptools google-cloud-speech google-cloud-videointelligence google-cloud-vision google-cloud-storage google-cloud-translate pyfiglet Werkzeug ffmpy six curl
+pip install --upgrade setuptools google-cloud-speech google-cloud-videointelligence google-cloud-vision google-cloud-storage google-cloud-translate pyfiglet Werkzeug ffmpy six
 
 # Verify ffmpeg and python are installed and in your path
 echo
